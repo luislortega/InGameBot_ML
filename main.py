52d0b24e605f509a835a987c47dca2930dcb3f22
luislortega <a18016323@alumnos.uady.mx>
2020-09-04 11:53:37 -0500

    draw a rectangle on the original image to detect where the image is most likely to be the object

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import cv2 as cv
 import numpy as np
 
-haystack_img = cv.imread('albion_farm.jpg', cv.IMREAD_UNCHANGED)
-needle_img = cv.imread('cabbage.jpg', cv.IMREAD_UNCHANGED)
+#import of images
+haystack_img = cv.imread('albion_farm.jpg', cv.IMREAD_REDUCED_COLOR_2)
+needle_img = cv.imread('cabbage.jpg', cv.IMREAD_REDUCED_COLOR_2)
 
 result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
 
@@ -18,7 +19,31 @@ print('Best match confidence: %s' % max_val)
 #Object found?
 threshold = 0.8
 
-if max_val > threshold
+if max_val > threshold:
     print('Found')
+    
+    #get dimension of the needle image
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+    
+    # 
+    #  (top_left)
+    #       *-------------------*
+    #       |                   |
+    #       |                   |
+    #       |                   |
+    #       |                   |
+    #       |                   |
+    #       *-------------------*
+    #                           (bottom_right)
+    top_left = max_loc
+    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+    
+    #draw the rectangle
+    cv.rectangle(haystack_img, top_left, bottom_right, color=(0,255,0), thickness=2, lineType=cv.LINE_4)
+    
+    #show the result
+    cv.imshow('Result', haystack_img)
+    cv.waitKey()
 else:
     print('Not found')
